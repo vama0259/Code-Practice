42e0b8908a341bf6a2b126d704c8801ca9bdf211
Varun Malhotra <73030002+vama0259@users.noreply.github.com>
2022-10-06 11:03:33 +0530

    Time: 35 ms (87.50%), Space: 43.2 MB (64.25%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -2,20 +2,28 @@ class Solution {
     public int numSquares(int n) {
         int dp[] = new int[n+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        return solve(n, dp);
-    }
-    public int solve(int n, int[] dp){
-        if(n<0) return Integer.MAX_VALUE;  //Taking min later on
-        if(n==0) return 0;
-        if(dp[n]!=Integer.MAX_VALUE){
-            return dp[n];
-        }
+        dp[0] = 0;
         
-        int min=n;
-        
-        for(int i=1;i*i<=n;i++){
-            min=Math.min(min,solve(n-(i*i), dp)+1);
+        for(int i =1; i <=n; i++){
+            int min = Integer.MAX_VALUE;
+            for(int j =1; j*j <=i; j++){
+                min = Math.min(min, dp[i-(j*j)]);
+            }
+            dp[i] = min+1;
         }
-        return dp[n] = min;
+        return dp[n];
     }
+//     public int solve(int n, int[] dp){
+//         if(n<0) return Integer.MAX_VALUE;  //Taking min later on
+//         if(n==0) return 0;
+//         if(dp[n]!=Integer.MAX_VALUE){
+//             return dp[n];
+//         }
+        
+//         int min=n;
+        
+//         for(int i=1;i*i<=n;i++){
+//             min=Math.min(min,solve(n-(i*i), dp)+1);
+//         }
+//         return dp[n] = min;
 }
